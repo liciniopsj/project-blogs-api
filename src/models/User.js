4f6cd8d9fd90417693b7c0adea65aa48946b3165
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-04-06 23:06:09 -0300

    model: User

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const user =  sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      tableName: 'users',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  user.associate = (models) => {
+    user.hasMany(models.BlogPost, {
+      foreignKey: 'id',
+      as: 'blogPosts',
+    });
+  };
+
+  return user;
+}
